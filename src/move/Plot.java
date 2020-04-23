7d041a5f589a04f1b7237d3f161f65e0fc255c0f
xuhan233 <xhan233@wisc.edu>
2020-04-22 22:19:48 -0500

    V1.5 Add plot of smoothing

--- src/move/Plot.java
+++ src/move/Plot.java
@@ -20,14 +20,14 @@ import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 
 public class Plot extends ApplicationFrame{
 
-
+    public static String walking = "C:\\Users\\Xu Han\\IdeaProjects\\Move\\src\\move\\WALKING.csv";
 
     public Plot(String applicationTitle, String chartTitle ) throws IOException {
         super(applicationTitle);
         JFreeChart xylineChart = ChartFactory.createXYLineChart(
                 chartTitle ,
-                "XPosition" ,
-                "YPosition" ,
+                "Time" ,
+                "Walking_x" ,
                 createDataset() ,
                 PlotOrientation.VERTICAL ,
                 true , true , false);
@@ -37,12 +37,12 @@ public class Plot extends ApplicationFrame{
         final XYPlot plot = xylineChart.getXYPlot( );
 
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer( );
-        renderer.setSeriesPaint( 0 , Color.MAGENTA );
-        //renderer.setSeriesPaint( 1 , Color.GREEN );
-        //renderer.setSeriesPaint( 2 , Color.YELLOW );
+        renderer.setSeriesPaint( 0 , Color.RED );
+        renderer.setSeriesPaint( 1 , Color.GREEN );
+        renderer.setSeriesPaint( 2 , Color.YELLOW );
         renderer.setSeriesStroke( 0 , new BasicStroke( 4.0f ) );
-        //renderer.setSeriesStroke( 1 , new BasicStroke( 3.0f ) );
-        //renderer.setSeriesStroke( 2 , new BasicStroke( 2.0f ) );
+        renderer.setSeriesStroke( 1 , new BasicStroke( 3.0f ) );
+        renderer.setSeriesStroke( 2 , new BasicStroke( 2.0f ) );
         plot.setRenderer( renderer );
         setContentPane( chartPanel );
     }
@@ -61,10 +61,34 @@ public class Plot extends ApplicationFrame{
             WalkingDTA.add(Move.Graphdateset().get(1).get(i) , Move.Graphdateset().get(0).get(i));
         }
 
+        final XYSeries Smooth_walking = new XYSeries("smooth_walking");
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        int dataset_size = (Move.Smoothdataoutput("walk", list, 0.01).get(0).size())/20;
+        for (int i = 1; i<dataset_size-1; i++) {
+            Smooth_walking.add(i,Move.Smoothdataoutput("walk", list, 0.01).get(0).get(i*20));
+        }
+
+        final XYSeries Origin_walking = new XYSeries("origin_walking");
+
+        int size = (Move.Smoothdataoutput("walk", list, 0.9).get(0).size())/20;
+        
+        for(int j = 1; j <size-1 ; j++) {
+
+            Origin_walking.add(j, Move.readData("C:\\Users\\Xu Han\\IdeaProjects\\Move\\src\\move\\WALKING.csv").get(1).get(j*20));
+        }
+
+
+
+
+
 
 
         final XYSeriesCollection dataset = new XYSeriesCollection( );
         dataset.addSeries(WalkingDTA );
+        dataset.addSeries(Smooth_walking);
+        dataset.addSeries(Origin_walking);
 
         return dataset;
     }
@@ -74,20 +98,19 @@ public class Plot extends ApplicationFrame{
         for(int a = 1; a < 10; a++) {
             smoothdatalist.clear();
             smoothdatalist.add(a);
-
-            System.out.println("Smooth data for Walking column: " + a + "\n" + Move.Smoothdataoutput("walk", smoothdatalist,0.9).get(0));
+            System.out.println("Smooth Walking data column: " + a + "\n" + Move.Smoothdataoutput("walk", smoothdatalist,0.6).get(0));
 
         }
         for(int b = 1;  b< 10; b++) {
             smoothdatalist.clear();
             smoothdatalist.add(b);
 
-            System.out.println("\nSmooth data for Turning column: " + b + "\n" + Move.Smoothdataoutput("turn", smoothdatalist,0.9).get(0));
+            System.out.println("\nSmooth Turning data column: " + b + "\n" + Move.Smoothdataoutput("turn", smoothdatalist,0.6).get(0));
         }
         for(int c = 1; c < 10; c++) {
             smoothdatalist.clear();
             smoothdatalist.add(c);
-            System.out.println("Smooth data for Walking and Turning column: " + c + "\n" + Move.Smoothdataoutput("w&t", smoothdatalist,0.9).get(0));
+            System.out.println("Smooth Walking and Turning data column: " + c + "\n" + Move.Smoothdataoutput("w&t", smoothdatalist,0.6).get(0));
         }
 
 
@@ -95,25 +118,25 @@ public class Plot extends ApplicationFrame{
         ArrayList<Integer> runlist = new ArrayList<Integer>();
 
         runlist.add(2);
-        //System.out.println("Smooth data for Walking\n" + Move.Smoothdataoutput("walk", runlist,0.9));
-        double step1 = Move.countingstep(Move.Smoothdataoutput("walk",runlist , 0.9).get(0));
-        double step2 = Move.countingstep(Move.Smoothdataoutput("w&t",runlist , 0.9).get(0));
-        System.out.println("\nStep From Walking: " + step1
-                + "\nStep From Walking & turning: " +step2
-                + "\nTotal Step: " + (step1 + step2) +"\n" );
+        System.out.println("Smooth data for Walking\n" + Move.Smoothdataoutput("walk", runlist,0.9));
+        double step1 = Move.countingstep(Move.Smoothdataoutput("walk",runlist , 0.01).get(0));
+        double step2 = Move.countingstep(Move.Smoothdataoutput("w&t",runlist , 0.01).get(0));
+        System.out.println("\nStep Walking: " + step1
+                + "\nStepS Walking and turning: " +step2
+                + "\nTotal Steps: " + (step1 + step2) +"\n" );
 
         runlist.remove(0);
         runlist.add(6);
         //System.out.println(runlist);
-        double t1 = Move.countingturn(Move.Smoothdataoutput("turn",runlist,0.9).get(0));
-        double t2 = Move.countingturn(Move.Smoothdataoutput("w&t",runlist,0.9).get(0));
-        System.out.println("Turning From Turning " + t1
-                + "\nTurning From Walking & Turning: "+ t2
-                + "\nTotal Turning " + (t1 + t2));
+        double t1 = Move.countingturn(Move.Smoothdataoutput("turn",runlist,0.01).get(0));
+        double t2 = Move.countingturn(Move.Smoothdataoutput("w&t",runlist,0.01).get(0));
+        System.out.println("Turning Turning " + t1
+                + "\nTurning  Walking and Turning: "+ t2
+                + "\nTotal Turnings " + (t1 + t2));
 
 
         Plot chart = new Plot("Browser Usage Statistics",
-                "PATH");
+                "Smoothchart");
         chart.pack( );
         RefineryUtilities.centerFrameOnScreen( chart );
         chart.setVisible( true );
